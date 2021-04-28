c8cc5ae22919bb2eff212e93b3f6be0ed9ff768e
kirin Thapar <kirinthapar@yandex.com>
2021-04-28 16:24:15 +0100

    color

--- src/containers/SignIn/SignIn.jsx
+++ src/containers/SignIn/SignIn.jsx
@@ -7,8 +7,7 @@ const SignIn = (props) => {
 
   return (
     <div className={styles.homePage}>
-      <h1>
-        PUNK-API</h1>
+      <h1>PUNK-API</h1>
         <h4> Sign in to search for your favourite beer</h4>
         <button onClick={signIn}>SIGN IN</button>
         
